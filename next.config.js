871df8a1e49eb4643339bc55207fd78b58e44b5d
John Sheehy <sheehy.john@gmail.com>
2023-11-22 12:04:09 +0000

    flickr image into realtime generative

--- next.config.js
+++ next.config.js
@@ -66,7 +66,7 @@ module.exports = () => {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
-      domains: ['picsum.photos', 'live.staticflickr.com', 'flic.kr'],
+      domains: ['picsum.photos', 'live.staticflickr.com'],
     },
     async headers() {
       return [
