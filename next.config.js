293eb30a54eb20c852326233152cb8ef2a15246b
John Sheehy <sheehy.john@gmail.com>
2024-02-25 07:21:39 +0000

    flickr works but mindomo doesn't even if published

--- next.config.js
+++ next.config.js
@@ -7,13 +7,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' codesandbox.io analytics.umami.is;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' codesandbox.io miro.com mindomo.com analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src codesandbox.io
+  frame-src codesandbox.io miro.com mindomo.com;
 `
 
  
